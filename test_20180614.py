5fb654f019514aaa84be2efb7d29c728eb1239c0
Avon Duan <ad@email.com>
2018-06-22 14:00:54 +0800

    20180614

---
+++ test_20180614.py
@@ -0,0 +1,79 @@
+# myfolat = 7.0
+# print(myfolat)
+# myfolat = float(7.0)
+# print(myfolat)
+# mystring= 'Hello'
+# print(mystring)
+# mystring = "Don't worry about that."
+# print(mystring)
+
+# h1 = 'hello'
+# h2 = 'world'
+# h3 = h1 + ' ' + h2
+# print(h3)
+
+# a, b = 3, 4
+# print(a, b)
+# mystring = "hello"
+# myfolat = 10.0
+# myint = 20
+# if mystring == "hello":
+#     print("String:%s " % mystring)
+# if isinstance(myfolat, float) and myfolat == 10.0:
+#     print("Float: %f" % myfolat)
+# if isinstance(myint, int) and myint == 20:
+#     print("Int:%d" % myint)
+# print(8 // 5)
+# print(17 / 3)
+# print(17 % 3)
+# print(5 *2 + 3)
+# print(2 ** 5, 5 * 2)
+# width = 20
+# height = 2 * 9
+# print(width * height)
+# print(4 * 3.75,)
+# tax = 12.5 / 100
+# price = 100.50
+# print(tax * price)
+# print('spam eggs')
+# # \ 用来转义'符号
+# print('dont\'s')
+# print("dont's")
+# print('"Yes,"he said')
+# print("\"Yes,\"he said")
+# print('"Isn\'t,"she said')
+# s = 'File line.\nSecond line.'
+# print(s)
+# s = r'C:\some\name'
+# print(s)
+# print("""\
+# Usage:thing
+#     -h  Dis
+#     -H hostname
+# """)
+# print(3 * 'number' + 5 * 'ok')
+# print('Py''thon')
+# a = 'Py'
+# print(a + 'thon')
+
+# text = ('Put several string within '
+#         'to have them joined together.')
+# print(text)
+
+# word = 'Python'
+# # print(word[0] + word[1] + word[2], word[3], word[-9])
+# # include left and excluded right(包括左边，但不包括右边)
+# # print(word[4:])
+# print(word[2:] + word[:2])
+
+# print('+---+---+---+---+---+---+\n')
+# print('+---+---+---+---+---+---+')
+
+# word = 'Python'
+# # print(word[0] )
+# # print('J' + word[1:])
+# print(word[:2])
+
+# s = 'dhashdjasdjasdandnjsadk'
+# print(len(s))
+# 开始看3.1.3列表,见2018年6月15日的py文件
