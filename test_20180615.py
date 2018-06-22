3560624602b9b4ef2aa803b309b9cb02be26f4b9
Avon Duan <ad@email.com>
2018-06-22 14:01:26 +0800

    20180615

---
+++ test_20180615.py
@@ -0,0 +1,70 @@
+# squares = [1, 4, 9, 10]
+# print(squares)
+# print(squares[:2])
+# print(squares[:2] + [32, 54, 13])
+# print(4 ** 3) # 求4的立方
+# cubes[3] = 64
+# print(cubes)
+# cubes_a = [1, 8, 27, 65, 125]
+# cubes_b = ['abc', 'ddas']
+# cubes_a.extend(cubes_b)
+# print(cubes_a)
+# cubes.append(216)
+# cubes.append(7 ** 2)
+# print(cubes)
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# print(letters)
+# letters[2:5] = ['C', 'D', 'E']
+# letters[2:5] = []
+# letters[:] = []
+# print(letters)
+
+# letters = ['a', 'b', 'c', 'd']
+# print(len(letters))
+
+# a = ['a', 'b', 'c']
+# n = [1, 2, 3]
+# x = [a, n]
+# # print(x)
+# print(x[0][1])
+# 斐波那契数
+# a, b = 0, 1
+# while b < 10:
+#     print(b)
+#     a, b = b, a + b
+#     # a=0,b=1,1
+#     # a=1,b=1,1
+#     # a=1,b=2,2
+#     # a=2,b=3,3
+#     # a=3,b=5,5
+#     # a=5,b=8,8
+#     # a=8,b=13,
+
+# i = 256 * 256
+# print('The value of i is', i)
+#
+# a, b = 0, 1
+# while b < 1000:
+#     print(b, end=',')
+#     a, b = b, a + b
+
+# s = input("Please enter an integer:")
+# x = int(s)
+# if x < 0:
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+# words = ['cat', 'windows', 'defenestrate']
+# for w in words[:]:
+#     if len(w) > 6:
+#         words.insert(0, w)
+#         print(words)
+# aLsit = [123, 'xyz', 'zara', 'abc']
+# aLsit.insert(2, 2009)
+# print(aLsit)
+# 从4.2开始看
