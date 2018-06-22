59e471f0c5ed6e866494b9efe8a7bb6599d3d5ca
Avon Duan <ad@email.com>
2018-06-22 14:02:57 +0800

    20180617-2

---
+++ test_20180617-2.py
@@ -0,0 +1,43 @@
+# name = input('Please enter your name:')
+# print("Hello", name)
+
+# print(1024 * 768)
+
+# print(10 / 2)
+# print(10 // 2)
+
+# d = {'Bob': 81, 'Mack': 99}
+# print(d['Tom'])
+
+# d = {}
+# # print(d['Tom'])
+# d.get('Tom', -1)
+# print(d[])
+
+# d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+# # print(d.get('Thomas', -1))
+# print(d.pop('Bob'))
+# print(d)
+
+# s = set([4, 100, 7, 3, 0, 2, 10])
+# s.add(55)
+# s.remove(4)
+# print(s)
+#
+# s1 = set([1, 2, 4])
+# s2 = set([2, 4, 7])
+# s3 = s1 & s2
+# print(s3)
+
+# s = ['c', 'b', 'a']
+# s.sort()
+# print(s)
+# a = 'abc'
+# print(a.replace('a', 'A'))
+# print(a)
+
+# t = (1, [2, 3])
+# t[1].pop()
+# print(t)
+
+'''从4.7.2开始看吧'''
