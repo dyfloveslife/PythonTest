680f14425cf5b9d75caa6baf6f8d8fec9db3267c
Avon Duan <ad@email.com>
2018-06-22 14:00:24 +0800

    20180330

---
+++ test_20180330.py
@@ -0,0 +1,48 @@
+# classmates = ['Mack', 'Tom', 'Jack', 'Carol']
+# print(classmates)
+# # print(len(classmates))
+# # print(classmates[1], classmates[2], classmates[0])
+# # print(classmates[-1])
+# # print(classmates[-2])
+# # print(classmates[-3])
+# # print(classmates[-4])
+# # classmates.append('Adam')
+# # classmates.append('Danna')
+# # classmates.insert(1, 'Pier')
+# # classmates.insert(111, '2')
+# # print(classmates)
+# # # print(len(classmates))
+# # # classmates.pop(1)
+# # print(classmates)
+# # print(classmates[0], classmates[1], classmates[2], classmates[3])
+# # classmates.pop()
+# classmates[0] = 'Sarch'
+# print(classmates)
+# L = [123, 'Apple', True]
+# print(L)
+# L = ['Apple', 456]
+# S = [123, L, 789]
+# print(S)
+# print(len(S))
+# print(S[1][1])
+# L = []
+# print(len(L))
+# classmates = ('Tom', 'Mack', 'Jack')
+# print(classmates)
+# print(len(classmates))
+# print(classmates[0], classmates[1], classmates[2], '\n', classmates[-1], classmates[-2])
+# t = ('a', 'b', ['A', 'B'])
+# t[2][0] = 'X'
+# t[2][1] = 'Y'
+# print(t)
+# print(len(t))
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
+# 打印Apple
+# print(L[0][0])
+# print(L[1][1])
+# 打印Lisa
+# print(L[-1][-1])
