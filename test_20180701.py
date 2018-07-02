3d959dc65dbaf32ddabf680fe039fef04a998e3b
Avon Duan <ad@email.com>
2018-07-02 11:11:28 +0800

    delete something

--- test_20180701.py
+++ test_20180701.py
@@ -60,7 +60,6 @@
 
 '''一边循环一边计算的机制，称为生成器：generator'''
 
-
 # L = [x * x for x in range(10)]  #list
 # print(L)
 # g = (x * x for x in range(1, 10, 2))  # generator
@@ -90,15 +89,6 @@
 # fib1(100)
 # fib2(100)
 
-# def fib(max):
-#     n, a, b = 0, 0, 1
-#     while n < max:
-#         yield b
-#         a, b = b, a + b
-#         n = n + 1
-#     return 'done'
-#
-# print(fib(10))
 
 # def odd():
 #     print('step1')
@@ -123,3 +113,47 @@
 #
 # for n in fib(6):
 #     print(n)
+
+# def fab(max):
+#     n, a, b = 0, 0, 1
+#     L = []
+#     while n < max:
+#         L.append(b)
+#         a, b = b, a + b
+#         n = n + 1
+#     return L
+#
+# for n in fab(5):
+#     print(n)
+
+'''
+yield 的作用就是把一个函数变成一个 generator，
+带有 yield 的函数不再是一个普通函数，Python 解释器会将其视为一个 generator，
+调用 fab(5) 不会执行 fab 函数，而是返回一个 iterable 对象！
+在 for 循环执行时，每次循环都会执行 fab 函数内部的代码，
+执行到 yield b 时，fab 函数就返回一个迭代值，下次迭代时，
+代码从 yield b 的下一条语句继续执行，
+而函数的本地变量看起来和上次中断执行前是完全一样的，
+于是函数继续执行，直到再次遇到 yield。
+'''
+'''
+一个带有 yield 的函数就是一个 generator，它和普通函数不同，
+生成一个 generator 看起来像函数调用，但不会执行任何函数代码，
+直到对其调用 next()（在 for 循环中会自动调用 next()）才开始执行。
+虽然执行流程仍按函数的流程执行，但每执行到一个 yield 语句就会中断，
+并返回一个迭代值，下次执行时从 yield 的下一个语句继续执行。
+看起来就好像一个函数在正常执行的过程中被 yield 中断了数次，
+每次中断都会通过 yield 返回当前的迭代值。
+'''
+
+
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield b
+#         a, b = b, a + b
+#         n = n + 1
+#     return 'done'
+#
+#
+# print(fib(10))
