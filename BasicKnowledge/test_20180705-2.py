c723782349ebb59619badf78f6297eb6ced667f9
Avon Duan <ad@email.com>
2018-07-05 20:35:52 +0800

    learning python basic grammar

---
+++ BasicKnowledge/test_20180705-2.py
@@ -0,0 +1,38 @@
+# import keyword
+# print(keyword.kwlist)
+
+# 放在开头的注释
+# print('Hello,world!')  # 放在语句后的注释
+# if True:
+#     print('True')
+# else:
+#     print('False')
+# print('hello1', end="-")
+# print("hello2")
+
+# print('hello\n')
+# print(r'hello\n')
+
+# print('Hello''abc')
+
+# print('a' + 'b' + 'c')
+# print('a' * 5)
+
+# str = 'IlovePyhton'
+# print('1:', str)  # 输出整个字符串
+# print('2:', str[:])  # 输出整个字符串
+# print('3:', str[0])  # 输出字符串第一个字符，其索引为0
+# print('4:', str[-1])  # 输出倒数第一的字符
+# print('5:', str[0:])  # 输出第一个到最后的所有字符
+# print('6:', str[:-1])  # 输出从第一个到倒数第二个字符，加了冒号之后就不包括右侧的索引位置了
+# print('7:', str[1:5])  # 输出从第二个到第五个字符，第二个字符的索引为1，第五个字符的索引为4，同样的，加了冒号之后就不包括右侧的索引位置了
+# print('8:', str + 'add')  # 用加号连接字符串
+
+# input('按下Enter后退出：')
+# print('helo"world')
+# print("hello'world")
+# import sys
+#
+# for i in sys.argv:
+#     print(i)
+# print('\n python 路径为', sys.path)
