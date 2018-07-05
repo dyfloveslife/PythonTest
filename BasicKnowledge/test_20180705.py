d56bca210a3c06a5384d0fe5a27849a9e9c4a41e
Avon Duan <ad@email.com>
2018-07-05 11:07:23 +0800

    20180705

---
+++ BasicKnowledge/test_20180705.py
@@ -0,0 +1,72 @@
+# import keyword
+# print(keyword.kwlist)
+'''
+第一行注释
+'''
+
+"""
+第二行注释
+"""
+
+# str = 'KeepOnCode'
+# print(str[2:5])
+# print(str * 2)
+# import sys
+#
+# x = 'Koc'
+# sys.stdout.writ(x + '\n')
+
+
+# import sys
+#
+# print('================Python import mode==========================');
+# print('命令行参数为：')
+# for i in sys.argv:
+#     print(i)
+# print('\n Python路径为', sys.path)
+
+# from sys import argv, path
+#
+# print('path', path)
+# a = 10
+# b = 20
+# if (a is b):
+#     print('a 和 b 有相同标志')
+# else:
+#     print('a 和 b 没有相同标志')
+# if (id(a) == id(b)):
+#     print('a 和 b 有相同标志')
+# else:
+#     print('a 和 b 没有相同标志')
+# var1 = 10
+# var2 = 2
+# del var1
+# print(var1)
+
+# print(17 % 3)
+# print(17 / 3)
+# print(17 // 3)  # 向下取整
+
+# print(round(3.141592, 3))
+
+
+import math
+
+# print(math.ceil(4.000000001))
+# print(abs(-10))
+# print(math.fabs(-10))
+# print(pow(2, 3))
+
+# import random
+# print(random.choice(range(10)))
+
+# from random import shuffle
+
+# list = [10, 22, 3, 0, -1, 10, 1]
+# shuffle(list)
+# print(list)
+# from random import uniform
+# a = uniform(1,2)
+# print(a)
+# print('\n'.join([' '.join(['%s*%s=%-2s' % (y, x, x * y) for y in range(1, x + 1)]) for x in range(1, 10)]))
+# print('\n'.join([''.join(['*'if abs((lambda a:lambda z,c,n:a(a,z,c,n))(lambda s,z,c,n:z if n==0else s(s,z*z+c,c,n-1))(0,0.02*x+0.05j*y,40))<2 else' 'for x in range(-80,20)])for y in range(-20,20)]))
