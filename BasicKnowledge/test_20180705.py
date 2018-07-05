d0ceae0e8f3617cf7aae971410002984c2534c7e
Avon Duan <ad@email.com>
2018-07-05 20:33:37 +0800

    delete something

--- BasicKnowledge/test_20180705.py
+++ BasicKnowledge/test_20180705.py
@@ -50,7 +50,7 @@
 # print(round(3.141592, 3))
 
 
-import math
+# import math
 
 # print(math.ceil(4.000000001))
 # print(abs(-10))
@@ -70,3 +70,4 @@ import math
 # print(a)
 # print('\n'.join([' '.join(['%s*%s=%-2s' % (y, x, x * y) for y in range(1, x + 1)]) for x in range(1, 10)]))
 # print('\n'.join([''.join(['*'if abs((lambda a:lambda z,c,n:a(a,z,c,n))(lambda s,z,c,n:z if n==0else s(s,z*z+c,c,n-1))(0,0.02*x+0.05j*y,40))<2 else' 'for x in range(-80,20)])for y in range(-20,20)]))
+
