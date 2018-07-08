26966cca36814c43db190657a0b51565146b2a97
Avon Duan <ad@email.com>
2018-07-08 17:16:34 +0800

    20180708

---
+++ BasicKnowledge/test_20180708.py
@@ -0,0 +1,50 @@
+# a = 10
+# b = 20
+# print(a and b)
+# print(a or b)
+# a = 0b101
+# b = 0o121
+# c = 0x121
+# print(a, b, c)
+# print(bin(b))
+# a = 1
+# b = 1
+# print(id(a))
+# print(id(b))
+# print(a is b)
+# print(round(10.4))
+# print(round(11.4))
+# print(0.1 + 0.1 + 0.1)
+# print(10 / 3)
+# print("我叫 %s 今年 %d 岁!" % ('小明', 10))
+# print("My name is %s,I'm %d years old." % ('Carol', 18))
+
+# var1 = "hello world"
+# print(var1[1: 3])
+
+# list = [1, 2, 3, 4, 5]
+# list.pop()
+# print(list)
+# list.append('banana')
+# print(list)
+
+
+# tup1 = (1, 2, 3, 4, 5)
+# print(tup1)
+# del tup1
+# print(tup1)
+# dic1 = {'name': 'Carol', 'gender': 'male', 'age': 18}
+# print(dic1)
+# del dic1['name']
+# print(dic1)
+# dic1.clear()
+# print(dic1)
+
+set1 = {1, 2, 3, 'a', 'b', 'c'}
+print(set1)
+set1.remove(1)
+print(set1)
+set1.discard(2)
+print(set1)
+set1.pop()
+print(set1)
