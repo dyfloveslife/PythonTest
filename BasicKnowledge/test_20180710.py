41a422367f833468b06eafe482a613de6cb99811
Avon Duan <ad@email.com>
2018-07-10 14:21:20 +0800

    20180910

---
+++ BasicKnowledge/test_20180710.py
@@ -0,0 +1,31 @@
+# def square(x):
+#     sum = 0
+#     for i in range(x):
+#         sum += x
+#         return sum
+#
+#
+# print(square(10))
+
+# some_dict = {}
+# some_dict[5.5] = "Ruby"
+# some_dict[5.0] = "JavaScript"
+# some_dict[5] = "Python"
+# print(some_dict[5.5])
+# print(some_dict[5.0])
+# print(some_dict[5])
+# print(hash(5) == hash(5.0))
+
+
+# print('somethin' is not None)
+# print('somethin' is (not None))
+
+# a = [1, 2, 3, 4]
+# b = a
+# a = a + [5, 6, 7, 8]
+# print(a, b)
+
+a = [1, 2, 3, 4]
+b = a
+a += [5, 6, 7, 8]
+print(a, b)
