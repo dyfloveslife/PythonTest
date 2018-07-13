d8612305a9a556d7ae0b0a5afc731a6622480572
Avon Duan <ad@email.com>
2018-07-13 09:44:40 +0800

    20180710

--- BasicKnowledge/test_20180710.py
+++ BasicKnowledge/test_20180710.py
@@ -25,7 +25,24 @@
 # a = a + [5, 6, 7, 8]
 # print(a, b)
 
-a = [1, 2, 3, 4]
-b = a
-a += [5, 6, 7, 8]
-print(a, b)
+# a = [1, 2, 3, 4]
+# b = a
+# a += [5, 6, 7, 8]
+# print(a, b)
+
+# import turtle
+# turtle.setup(1000, 500, 200, 200)
+# turtle.penup()
+# turtle.fd(-250)
+# turtle.pendown()
+# turtle.pensize(25)
+# turtle.pencolor('pink')
+# turtle.seth(-40)
+# for i in range(5):
+#     turtle.circle(40, 80)
+#     turtle.circle(-40, 80)
+# turtle.circle(40, 80 / 2)
+# turtle.fd(40)
+# turtle.circle(40, 180)
+# turtle.fd(40 * 2 / 3)
+# turtle.done()
